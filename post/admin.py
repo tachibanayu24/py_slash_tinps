02a392750983e3500e1afa8ae696cece80e71e27
gonkunkun <yuta.sasaki.ipu@gmail.com>
2018-09-16 13:36:45 +0900

    fix db

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import User, Tinps, Category
+from .models import Tinps, Category
 
-
-admin.site.register(User)
 admin.site.register(Tinps)
 admin.site.register(Category)
