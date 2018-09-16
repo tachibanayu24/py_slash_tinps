e36c806b0119dbbd65af98dc0b9016dd89d01c23
Yuto Tachibana <tachibanayu24@gmail.com>
2018-09-16 12:35:47 +0900

    create views post edit

--- post/views.py
+++ post/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def post(request):
-    return HttpResponse("<h1>#post</h1>")
+    return render(request, 'post/index.html')
 
 
 def edit(request):
-    return HttpResponse("<h1>#edit</h1>")
+    return render(request, 'edit/index.html')
