a1a0fd42eb5436dc7f69de898bf0da68e603bcfe
gonkunkun <yuta.sasaki.ipu@gmail.com>
2018-09-17 17:05:03 +0900

    インサート処理追加

--- post/views.py
+++ post/views.py
@@ -23,12 +23,13 @@ def edit(request):
     return render(request, 'edit/index.html', context)
 
 
-def update(request):
+def insert(request):
     print (request.POST)
     req = request.POST
 
     # ユーザ様
     user = User(
+        user_id="630552b028414439ad3a52c931781044",
         user_name="",
         mail="",
         pwd=""
@@ -46,4 +47,34 @@ def update(request):
     )
     query.save()
 
-    return render(request, 'detail/index.html')
+    return render(request, 'show/index.html')
+
+
+
+def update(request):
+    print (request.POST)
+    req = request.POST
+
+    # ユーザ様
+    user = User(
+        user_id="630552b028414439ad3a52c931781044",
+        user_name="",
+        mail="",
+        pwd=""
+    )
+
+    # tinps用
+    title = req["title"]
+    tinps_body = req["tinps_body"]
+
+    query = Tinps(
+        title=title,
+        tinps_body=tinps_body,
+        user_name=user,
+        #category
+    )
+    query.save()
+
+    return render(request, 'show/index.html')
+
+
