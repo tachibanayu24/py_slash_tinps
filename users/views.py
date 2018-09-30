767c159d331c1aed0d4d1b90ebe8daa897c031ec
gonkunkun <yuta.sasaki.ipu@gmail.com>
2018-09-30 16:36:29 +0900

    merge db and fix post

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from users.models import CustomUser
+from post.models import Tinps
 
-# Create your views here.
+def mypage(request):
+    #　本来はログイン中のユーザのみ渡すはず
+    user = request.user
+
+    context = {
+        'tinps_list':Tinps.objects.filter(user_id=user.id).all(),
+        'user': user,
+    }
+    return render(request, 'mypage/index.html', context)
+
+
+def register(request):
+    #　本来はログイン中のユーザのみ渡すはず
+    context = {
+        'tinps_list':Tinps.objects.all(),
+        'user_list': CustomUser.objects.all(),
+    }
+    return render(request, 'register/index.html', context)
+
