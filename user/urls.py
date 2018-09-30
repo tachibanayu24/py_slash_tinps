cde5be645adb849972d007e7deee9d6afd209818
gonkunkun <yuta.sasaki.ipu@gmail.com>
2018-09-30 16:38:40 +0900

    merge db and fix post

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('mypage/', views.mypage, name='mypage'),
-    path('register/', views.register, name='register'),
-]
